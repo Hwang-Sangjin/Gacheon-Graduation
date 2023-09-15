fafbd23c9ee7c9e87478e3b10b3a99516f7ff789
황상진 <sangjinh@mobiltech.io>
2023-09-15 20:31:39 +0900

    add: react three fiber page

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Suspense, useState } from "react";
+
+import Bananas from "./components/Bananas";
+import { FadeIn, LeftMiddle } from "./layout/styles";
+import Overlay from "./layout/Overlay";
+
+function App() {
+  const [speed, set] = useState(1);
+  return (
+    <>
+      <Suspense fallback={null}>
+        <Bananas speed={speed} />
+        <FadeIn />
+      </Suspense>
+      <Overlay />
+      <LeftMiddle>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={speed}
+          step="1"
+          onChange={(e) => set(e.target.value)}
+        />
+      </LeftMiddle>
+    </>
+  );
+}
+
+export default App;
