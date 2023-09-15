07151b0c99e4dba44a99b855f47a6ddfcd42e2f6
황상진 <sangjinh@mobiltech.io>
2023-09-15 21:54:17 +0900

    add: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Suspense, useState } from "react";
-
+import Header from "./components/Header";
 import Bananas from "./components/Bananas";
 import { FadeIn, LeftMiddle } from "./layout/styles";
 import Overlay from "./layout/Overlay";
@@ -8,6 +8,7 @@ function App() {
   const [speed, set] = useState(1);
   return (
     <>
+      <Header />
       <Suspense fallback={null}>
         <Bananas speed={speed} />
         <FadeIn />
