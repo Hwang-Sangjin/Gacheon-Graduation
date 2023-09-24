5cdfe612aab0141b45771ab04f91242129d3a36a
Sangjin Hwang <aosdb5640@naver.com>
2023-09-24 22:01:57 +0900

    add: first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import { BrowserRouter } from "react-router-dom";
 import {
-  About,
-  Contact,
-  Experience,
-  Feedbacks,
-  Hero,
+
   Navbar,
-  Tech,
-  Works,
-  StarsCanvas,
+
 } from "./components";
 
 const App = () => {
@@ -17,15 +11,7 @@ const App = () => {
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        <div classNAme="relative z-0">
-          <Contact />
-          <StarsCanvas />
+          
         </div>
       </div>
     </BrowserRouter>
