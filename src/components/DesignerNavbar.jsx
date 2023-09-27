9d7f38bab037b779f21dc927e595a2f46ae28306
황상진 <sangjinh@mobiltech.io>
2023-09-27 23:24:03 +0900

    add: designer nav bar

--- src/components/DesignerNavbar.jsx
+++ src/components/DesignerNavbar.jsx
@@ -1,7 +1,39 @@
 import React, { useEffect, useState } from "react";
 
-const DesignerNavbar = () => {
-  return <div>All comittee Product Space</div>;
+const DesignerNavbar = ({ selectedCategory, setSelectedCategory }) => {
+  const ChangeCategory = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const catetories = ["All", "Comittee", "Product", "Space"];
+
+  return (
+    <div>
+      <ul className="flex flex-row">
+        {catetories.map((category) => {
+          return category === selectedCategory ? (
+            <div
+              className="mx-1 my-2 bg-black flex px-4 py-0.5 rounded-md"
+              key={category.id}
+              onClick={() => ChangeCategory(category)}
+            >
+              <li key={category.id} className="text-[#FFF16F]">
+                ({category})
+              </li>
+            </div>
+          ) : (
+            <div
+              className="mx-1 my-2 flex px-4 py-0.5"
+              key={category.id}
+              onClick={() => ChangeCategory(category)}
+            >
+              <li key={category.id}>({category})</li>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default DesignerNavbar;
--- src/pages/Designers.jsx
+++ src/pages/Designers.jsx
@@ -3,15 +3,22 @@ import { motion } from "framer-motion";
 import { fadeIn, textVariant } from "../utils/motion";
 import { styles } from "../styles";
 import DesignerNavbar from "../components/DesignerNavbar";
+import { useState, useEffect } from "react";
 
 const Designers = () => {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  useEffect(() => {
+    console.log(selectedCategory);
+  }, [selectedCategory]);
+
   return (
     <div className="relative z-0 bg-primary">
       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
         <Navbar />
         <section className="relative w-full h-screen mx-auto">
           <div
-            className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+            className={`${styles.paddingX} relative inset-0 top-[120px] max-w-7xl mx-auto flex flex-col items-start gap-5`}
           >
             <motion.div variants={textVariant()}>
               <h2 className={`${styles.sectionHeadText}`}>Designers</h2>
@@ -20,6 +27,10 @@ const Designers = () => {
                 Exhibition
               </p>
             </motion.div>
+            <DesignerNavbar
+              selectedCategory={selectedCategory}
+              setSelectedCategory={setSelectedCategory}
+            />
           </div>
         </section>
       </div>
