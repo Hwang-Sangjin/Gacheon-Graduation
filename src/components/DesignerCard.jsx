a9217c234032e39a7e2a3a657ab1d9521759847b
황상진 <sangjinh@mobiltech.io>
2023-10-05 21:54:27 +0900

    add: insta, behance url

--- src/components/DesignerCard.jsx
+++ src/components/DesignerCard.jsx
@@ -1,14 +1,17 @@
-const DesignerCard = ({title, data}) => {
-    return(
-        <div>
-            <div>
-                {title}
-            </div>
-            <div>
-                {data.Name_kor}
-            </div>
-        </div>
-    )
-}
+const DesignerCard = ({ title, data }) => {
+  return (
+    <div>
+      <div>{title}</div>
+      <div className="w-1/4">
+        <img src={data.Image_BW} />
+        <div>{data.Name_kor}</div>
+        <div>{data.Name_eng}</div>
+        <div>{data.Role}</div>
+        <div>Email {data.Email}</div>
+        <div>Link {data.Name_kor}</div>
+      </div>
+    </div>
+  );
+};
 
-export default DesignerCard
+export default DesignerCard;
