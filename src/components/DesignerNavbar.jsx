ca4382aff499d77dade6c54c96c99edd1e5e3659
황상진 <sangjinh@mobiltech.io>
2023-10-06 21:37:19 +0900

    add: change font size

--- src/components/DesignerNavbar.jsx
+++ src/components/DesignerNavbar.jsx
@@ -17,7 +17,7 @@ const DesignerNavbar = ({ selectedCategory, setSelectedCategory }) => {
               key={category.id}
               onClick={() => ChangeCategory(category)}
             >
-              <li key={category.id} className="text-[#FFF16F]">
+              <li key={category.id} className="text-[#FFF16F] text-3xl">
                 ({category})
               </li>
             </div>
@@ -27,7 +27,9 @@ const DesignerNavbar = ({ selectedCategory, setSelectedCategory }) => {
               key={category.id}
               onClick={() => ChangeCategory(category)}
             >
-              <li key={category.id}>({category})</li>
+              <li key={category.id} className="text-3xl">
+                ({category})
+              </li>
             </div>
           );
         })}
