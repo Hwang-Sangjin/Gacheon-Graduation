027983cd212ee6d09631098fee6cfb6f764a709c
황상진 <sangjinh@mobiltech.io>
2023-10-06 21:34:33 +0900

    add: change designer card css

--- src/components/DesignerCard.jsx
+++ src/components/DesignerCard.jsx
@@ -1,9 +1,9 @@
 const DesignerCard = ({ title, data }) => {
   return (
     <div>
-      <div>{title}</div>
-      <div className="md:w-2/3 sm:w-1/3">
-        <img src={data.Image_BW} />
+      <div className="h-[2rem] text-3xl mb-4">{title}</div>
+      <div className="md:w-3/4 sm:w-1/5">
+        <img className="designer-card" src={data.Image_Color} />
         <div>{data.Name_kor}</div>
         <div>{data.Name_eng}</div>
         <div>{data.Role}</div>
