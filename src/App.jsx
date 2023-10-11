e2e896d2fb6f7ebd373290344e99cda77bcab8a1
황상진 <sangjinh@mobiltech.io>
2023-10-11 13:45:05 +0900

    add: custom cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import {
   createBrowserRouter,
   RouterProvider,
-  BrowserRouter
+  BrowserRouter,
 } from "react-router-dom";
 
 import Main from "./pages/Main";
@@ -9,6 +9,8 @@ import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Designers from "./pages/Designers";
 import Visit from "./pages/Visit";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 const router = createBrowserRouter([
   {
@@ -34,9 +36,35 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  useEffect(() => {
+    const mouseMove = (e) => {
+      setMousePosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener("mousemove", mouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
+
+  const variants = {
+    default: {
+      x: mousePosition.x - 10,
+      y: mousePosition.y - 10,
+      rotate: 45,
+    },
+  };
+
   return (
     <>
       <RouterProvider router={router} />
+      <motion.div className="cursor" variants={variants} animate="default" />
     </>
   );
 };
