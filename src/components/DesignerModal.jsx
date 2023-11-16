d34f6e0c430e575e325a1d44a782e246b2e12eda
황상진 <sangjinh@mobiltech.io>
2023-11-16 15:58:17 +0900

    add: modal design modify

--- src/components/DesignerModal.jsx
+++ src/components/DesignerModal.jsx
@@ -1,64 +1,98 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-
+import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
-
-
+import { ProductData } from "../constants";
 
 const dropIn = {
-    hidden: {
-      y: "-100vh",
-      opacity: 0,
+  hidden: {
+    y: "-100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: "0",
+    opacity: 1,
+    transition: {
+      duration: 0.1,
+      type: "spring",
+      damping: 25,
+      stiffness: 500,
     },
-    visible: {
-      y: "0",
-      opacity: 1,
-      transition: {
-        duration: 0.1,
-        type: "spring",
-        damping: 25,
-        stiffness: 500,
-      },
-    },
-    exit: {
-      y: "100vh",
-      opacity: 0,
-    },
-  };
+  },
+  exit: {
+    y: "100vh",
+    opacity: 0,
+  },
+};
 
-const DesignerModal = ({data}) => {
-    return(
-
-        <motion.div
-          onClick={(e) => e.stopPropagation()}
-          className="relative rounded-2xl shadow dark:bg-gray-700 flex flex-row"
-          variants={dropIn}
-          initial="hidden"
-          animate="visible"
-          exit="exit"
-        >
-          <img src={data.Image_Full} className="w-80">
-            
-          </img>
-          <div className="flex flex-col rounded-2xl w-80 bg-[#FFF16F]">
-            <div className="inline-flex rounded-xl justify-center m-4 px-7 w-12 font-['Pretendard-Medium'] bg-white">ICON</div>
-            <div className="basis-1/2 ">1</div>
-            <div className="basis-1/2">2</div>
-          </div>
-          <div className="flex flex-col w-[40rem] ">
+const DesignerModal = ({ data }) => {
+  console.log(data);
+  console.log(data.Image_Project1 === "", data.Image_Project2 === "");
+  return (
+    <motion.div
+      onClick={(e) => e.stopPropagation()}
+      className="relative rounded-2xl shadow dark:bg-gray-700 flex flex-row"
+      variants={dropIn}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
+      <img src={data.Image_Full} className="w-80"></img>
+      <div className="flex flex-col rounded-2xl w-80 bg-[#FFF16F]">
+        <div className="inline-flex rounded-xl justify-center m-4 px-7 w-12 font-['Pretendard-Medium'] bg-white">
+          ICON
+        </div>
+        <div className="basis-1/2 ">
+          <img src={data.Image_Icon} />
+        </div>
+        <div className="basis-1/2">2</div>
+      </div>
+      <div className="flex flex-col w-[40rem] ">
+        {data.Image_Project1 === "" && data.Image_Project2 === "" ? (
+          <>
+            <div className="basis-1 rounded-2xl bg-[#E4E4E4]">
+              <div className="inline-flex rounded-xl justify-center m-4 px-7 w-21 font-['Pretendard-Medium'] bg-white">
+                COMMENT
+              </div>
+              <div className="h-full ">1</div>
+            </div>
+          </>
+        ) : (
+          <>
             <div className="basis-1/2 rounded-2xl bg-[#E4E4E4]">
-              <div className="inline-flex rounded-xl justify-center m-4 px-7 w-12 font-['Pretendard-Medium'] bg-white">COMMENT</div>
+              <div className="inline-flex rounded-xl justify-center m-4 px-5 w-21 font-['Pretendard-Medium'] bg-white">
+                COMMENT
+              </div>
               <div>1</div>
             </div>
-            <div className="basis-1/2 rounded-2xl flex bg-[#BEBEBE]">
-              <div className="basis-1/2">1</div>
-              <div className="basis-1/2">2</div>
+            <div className="basis-1/2 rounded-2xl  flex-col bg-[#BEBEBE]">
+              <div className="inline-flex rounded-xl m-4 px-5 font-['Pretendard-Medium'] bg-white">
+                PROJECT
+              </div>
+              <div className="inline-flex flex-row">
+                <div className="basis-1/2 p-4">
+                  <Tilt>
+                    <img src={data.Image_Project_Product} />
+                  </Tilt>
+                  <div className="text-[#171717] text-lg font-['Pretendard-SemiBold'] mt-3">
+                    {data.Title_Product}
+                  </div>
+                </div>
+                <div className="basis-1/2 p-4">
+                  <Tilt>
+                    <img src={data.Image_Project_Space} />
+                  </Tilt>
+                  <div className="text-[#171717] text-lg font-['Pretendard-SemiBold'] mt-3">
+                    {data.Title_Space}
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        </motion.div>
-       
-    )
-} 
+          </>
+        )}
+      </div>
+    </motion.div>
+  );
+};
 
-export default DesignerModal
-
+export default DesignerModal;
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,13 +2,12 @@ import { motion } from "framer-motion";
 import { fadeIn, textVariant } from "../utils/motion";
 import { Tilt } from "react-tilt";
 import { useNavigate } from "react-router-dom";
-import {useState} from "react";
+import { useState } from "react";
 
-const ProjectCard = ({  type, data, index }) => {
+const ProjectCard = ({ type, data, index }) => {
   const [text, setText] = useState(false);
   const navigate = useNavigate();
   return (
-    
     <Tilt className="w-full mb-10">
       <motion.div
         variants={fadeIn("right", "spring", index * 0.5, 0.75)}
@@ -17,20 +16,33 @@ const ProjectCard = ({  type, data, index }) => {
         viewport={{ once: true }}
         className="w-full rounded-[20px]"
         whileHover={() => setText(true)}
-        onHoverEnd={()=> setText(false)}
-        onClick={()=>{
-          navigate('/personal',{
-          state: {
-            data: data,
-            type: type
-          }
-        })}}
+        onHoverEnd={() => setText(false)}
+        onClick={() => {
+          navigate("/personal", {
+            state: {
+              data: data,
+              type: type,
+            },
+          });
+        }}
       >
         <div className="w-3/4 w-1/5 relative text-center">
-            {type==="product"? 
-            <img className="project-card w-full h-full" src={data.Image_Product} />: 
-            <img className="project-card w-full h-full" src={data.Image_Space} />}
-            {text? <div className="text-[#FFFFFF] text-2xl font-['Hack-Regular'] project-card-text">{type==="product"? data.Title_Product: data.TitleSpace}</div>:null}
+          {type === "product" ? (
+            <img
+              className="project-card w-full h-full"
+              src={data.Image_Product}
+            />
+          ) : (
+            <img
+              className="project-card w-full h-full"
+              src={data.Image_Space}
+            />
+          )}
+          {text ? (
+            <div className="text-[#FFFFFF] text-2xl font-['Hack-Regular'] project-card-text">
+              {type === "product" ? data.Title_Product : data.Title_Space}
+            </div>
+          ) : null}
         </div>
       </motion.div>
     </Tilt>
