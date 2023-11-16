446ab179b0a9f139ad69a52bd3b2ac8f27513472
황상진 <sangjinh@mobiltech.io>
2023-11-16 17:42:54 +0900

    add: 3d model

--- src/components/Earth.jsx
+++ src/components/Earth.jsx
@@ -1,53 +1,29 @@
-import React, { Suspense, useRef, useState } from "react";
-import { Canvas, useFrame,useLoader  } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF, useFBX} from "@react-three/drei";
+import React, { Suspense, useEffect, useRef, useState } from "react";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import {
+  OrbitControls,
+  Environment,
+  Preload,
+  useGLTF,
+  useFBX,
+} from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import CanvasLoader from "./Loader";
 
-const Earth = () => {
-  const earth = useGLTF("./GLTF/Gear1.gltf");
-  return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
-  );
-};
-
-const KimGoEun = () => {
-  const gltf = useLoader(GLTFLoader, "./GLTF/KimGoEun.gltf")
-  return <primitive object={gltf.scene} />
-};
-
-function Box(props) {
-  const KimGoEun_model = useGLTF("./GLTF/KimGoEun.gltf")
-  // This reference will give us direct access to the mesh
-  const meshRef = useRef()
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (meshRef.current.rotation.x += delta))
-  // Return view, these are regular three.js elements expressed in JSX
-  return (
-    <mesh
-      {...props}
-      ref={meshRef}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
-}
-
 const EarthCanvas = () => {
+  const Scene = () => {
+    const fbx = useFBX("./FBX/KimDongJun/KimDongJun_model.fbx");
+
+    return <primitive object={fbx} scale={0.005} position={[0, 0, 0]} />;
+  };
+
   return (
     <Canvas>
-        <Suspense fallback={null}>
-          <KimGoEun />
-          <OrbitControls />
-        </Suspense>
-      </Canvas>
+      <Suspense fallback={null}>
+        <Scene />
+        <OrbitControls />
+      </Suspense>
+    </Canvas>
   );
 };
 
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -10,9 +10,14 @@ import MainImageSequence from "../MainSequence";
 import StarsCanvas from "../components/StarsCanvas";
 import { slideIn } from "../utils/motion";
 import EarthCanvas from "../components/Earth";
+import { Canvas } from "react-three-fiber";
+import { BoxGeometry, MeshStandardMaterial } from "three";
+import { useFBX } from "@react-three/drei";
 
 const Main = () => {
   const [loading, setLoading] = useState(false);
+
+  const fbx = useFBX("./FBX/KimDongJun/KimDongJun_model.fbx");
 
   useEffect(() => {
     setLoading(true);
@@ -28,7 +33,7 @@ const Main = () => {
         <div className="relative z-0 bg-[#000000] h-full">
           <Controller>
             <Scene
-            className="scrollmagic-pin-spacer-main"
+              className="scrollmagic-pin-spacer-main"
               duration="10000"
               triggerHook="onLeave"
               pin={{ pushFollowers: false }}
@@ -45,8 +50,11 @@ const Main = () => {
             variants={slideIn("right", "tween", 0.2, 1)}
             className="main-canvas"
           >
-            <EarthCanvas />
-            <StarsCanvas />
+            <Canvas>
+              <ambientLight intensity={0.1} />
+              <directionalLight color="red" position={[0, 0, 5]} />
+              <primitive object={fbx} />
+            </Canvas>
           </motion.div>
         </div>
       )}
